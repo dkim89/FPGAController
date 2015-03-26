c0092d14227f0f62a38e2c42f5ced38298f33a4a
dkim89 <dkim89@bu.edu>
2015-03-26 13:25:28 -0400

    Revert to no bluetooth

--- app/src/main/java/spring15/ec551/fpgacontroller/MainActivity.java
+++ app/src/main/java/spring15/ec551/fpgacontroller/MainActivity.java
@@ -1,34 +1,22 @@
 package spring15.ec551.fpgacontroller;
 
 import android.app.FragmentTransaction;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothServerSocket;
-import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.inputmethodservice.Keyboard;
-import android.os.Handler;
-import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputMethod;
 import android.view.inputmethod.InputMethodInfo;
 import android.view.inputmethod.InputMethodManager;
-
-import com.bda.controller.Controller;
 
 import java.util.List;
 
 public class MainActivity extends ActionBarActivity implements MenuInterfaceListener {
-//        BluetoothConnectionListener {
 
-    Controller object;
 //    public static UUID mUUID;
 //    public static String mServiceString = "application_server";
 //    public static BluetoothAdapter mBluetoothAdapter;
@@ -38,16 +26,13 @@ public class MainActivity extends ActionBarActivity implements MenuInterfaceList
 //    AcceptThread mAcceptThread;
 //
 //    ConnectThread mConnectThread;
+//    private static String MOGA_UNIVERSAL_PACKAGE = "net.obsidianx.android.mogaime";
 
+    private final String SETTINGS_FRAGMENT_CHANGE = "Settings_Fragment";
 
     View mControllerIndicator;
     View mVehicleIndicator;
     final String MENU_FRAGMENT = "Menu Fragment";
-    private InputMethodManager mInputMethodManager;
-    private Keyboard mKeyboard;
-    private static String MOGA_UNIVERSAL_PACKAGE = "net.obsidianx.android.mogaime";
-
-    private final String SETTINGS_FRAGMENT_CHANGE = "Settings_Fragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,17 +45,15 @@ public class MainActivity extends ActionBarActivity implements MenuInterfaceList
         mVehicleIndicator = findViewById(R.id.vehicle_indicator);
 
         // Checks for the Moga Universal Driver
-        mInputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        List<InputMethodInfo> mInputMethodProperties = mInputMethodManager.getEnabledInputMethodList();
-
-        final int N = mInputMethodProperties.size();
-
-        for (int i=0; i < N; i++) {
-            InputMethodInfo imi = mInputMethodProperties.get(i);
-            if (imi.getPackageName().equals(MOGA_UNIVERSAL_PACKAGE)) {
-                isControllerDetected();
-            }
-        }
+//        mInputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//        List<InputMethodInfo> mInputMethodProperties = mInputMethodManager.getEnabledInputMethodList();
+//        final int N = mInputMethodProperties.size();
+//        for (int i=0; i < N; i++) {
+//            InputMethodInfo imi = mInputMethodProperties.get(i);
+//            if (imi.getPackageName().equals(MOGA_UNIVERSAL_PACKAGE)) {
+//                isControllerDetected();
+//            }
+//        }
 
 //        pairedDevices = BluetoothAdapter.getDefaultAdapter().getBondedDevices();
 //        if (pairedDevices.size() > 0) {
@@ -125,22 +108,16 @@ public class MainActivity extends ActionBarActivity implements MenuInterfaceList
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    public void isControllerDetected() {
-            mControllerIndicator.setBackgroundResource(R.drawable.status_not_calibrated);
-    }
-
-    public void isControllerCalibrated() {
-        mControllerIndicator.setBackgroundResource(R.drawable.status_connected);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+//    public void isControllerDetected() {
+//            mControllerIndicator.setBackgroundResource(R.drawable.status_not_calibrated);
+//    }
+//
+//    public void isControllerCalibrated() {
+//        mControllerIndicator.setBackgroundResource(R.drawable.status_connected);
+//    }
 
 //    @Override
 //    public void onSuccessfulPairing(BluetoothServerSocket socket) {
